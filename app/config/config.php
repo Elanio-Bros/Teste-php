bdf3fed386d9d5e5f3d290c38674e1568197be24
Elanio-Bros <elaniobros@gmail.com>
2021-12-09 08:35:44 -0300

    Criando estrutura do código com padrão MVC

---
+++ app/config/config.php
@@ -0,0 +1,4 @@
+<?php
+define('BASE', '../../');
+define('UNSET_UTI_COUNT', 1);
+define('DEBUG_URI', true);
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use app\controller\TesteController;
+
+$controler = new TesteController();
