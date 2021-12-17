4bb1693512d5056f85f3b0195363222bc3322a0f
Elanio-Bros <elaniobros@gmail.com>
2021-12-17 08:46:47 -0300

    Ajustes no config para sobre erro

--- app/config/config.php
+++ app/config/config.php
@@ -4,14 +4,19 @@ use App\Core\Messagem;
 
 try {
     Dotenv\Dotenv::createUnsafeImmutable(file_exists('./.env') ? './' : '../')->load();
+    define('BASE', '/Teste-php-via-maquinas/');
+    define('DBDRIVER', (getenv('DB_CONNECTION') != null ? getenv('DB_CONNECTION') : 'mysql'));
+    define('DBHOST', (getenv('DB_HOST') != null ? getenv('DB_HOST') : '127.0.0.1')  . ":" . (getenv('DB_PORT') != null ? getenv('DB_PORT') : '3306'));
+    define('DBNAME', (getenv('DB_DATABASE') != null ? getenv('DB_DATABASE') : 'mysql'));
+    define('DBUSER', (getenv('DB_USERNAME') != null ? getenv('DB_USERNAME') : 'root'));
+    define('DBPASS', (getenv('DB_PASSWORD') != null ? getenv('DB_PASSWORD') : ''));
+    define('TIME_ZONE', (getenv('TIME_ZONE') != null ?  getenv('TIME_ZONE') : 'America/Sao_Paulo'));
+    date_default_timezone_set(TIME_ZONE);
 } catch (Exception $e) {
-    (new Messagem)->errorHttp(500, "Arquivo .env não encontrado", $e);
+    if (isset($_SERVER['REQUEST_URI'])) {
+        (new Messagem)->errorHttp(500, "Arquivo .env não encontrado", $e);
+    } else {
+        echo "Arquivo .env não encontrado";
+    }
+    die();
 }
-define('BASE', '/Teste-php-via-maquinas/');
-define('DBDRIVER', (getenv('DB_CONNECTION') != null ? getenv('DB_CONNECTION') : 'mysql'));
-define('DBHOST', (getenv('DB_HOST') != null ? getenv('DB_HOST') : '127.0.0.1')  . ":" . (getenv('DB_PORT') != null ? getenv('DB_PORT') : '3306'));
-define('DBNAME', (getenv('DB_DATABASE') != null ? getenv('DB_DATABASE') : 'mysql'));
-define('DBUSER', (getenv('DB_USERNAME') != null ? getenv('DB_USERNAME') : 'root'));
-define('DBPASS', (getenv('DB_PASSWORD') != null ? getenv('DB_PASSWORD') : ''));
-define('TIME_ZONE', (getenv('TIME_ZONE') != null ?  getenv('TIME_ZONE') : 'America/Sao_Paulo'));
-date_default_timezone_set(TIME_ZONE);
